92402a88da102536aee5cd428943dbfed4f91df3
Laurens54321 <laurens.hons@gmail.com>
2022-11-24 23:56:00 +0100

    vibration feature implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour
     
     public void GameOver()
     {
+        Handheld.Vibrate();
         gameOverCanvas.SetActive(true);
         Time.timeScale = 0;
     }
